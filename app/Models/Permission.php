fee4ebeb3e9a6c921d2f75120e4d09c8fef5bf5d
VU HUY HOANG <v_huyhoang@thk-hd.vn>
2026-01-14 15:29:59 +0700

    Update permission model

---
+++ app/Models/Permission.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Permission as ModelsPermission;
+
+class Permission extends ModelsPermission
+{
+    //
+	protected $fillable = [
+		'name',
+		'guard_name',
+		'description'
+	];
+}
